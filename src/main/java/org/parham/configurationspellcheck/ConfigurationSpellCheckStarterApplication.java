17f86ecd08918bebfa283f6c241dfefa29b560b8
ParhamTheManiac <52646639+parhamahmady@users.noreply.github.com>
2022-03-04 00:20:44 +0330

    initialize

---
+++ src/main/java/org/parham/configurationspellcheck/ConfigurationSpellCheckStarterApplication.java
@@ -0,0 +1,13 @@
+package org.parham.configurationspellcheck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigurationSpellCheckStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigurationSpellCheckStarterApplication.class, args);
+    }
+
+}
